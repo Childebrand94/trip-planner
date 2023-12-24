467ee863b3fb1b7418a93b8028a4cdbbaf51b7cb
Chris Hildebrand <hildebrandc94@gmail.com>
2023-12-24 15:20:22 -0500

    fix: itinerary votes will up date on current page.

--- app/controllers/itinerary_votes_controller.rb
+++ app/controllers/itinerary_votes_controller.rb
@@ -1,11 +1,9 @@
 class ItineraryVotesController < ApplicationController
   before_action :set_itinerary_item
-  before_action :set_date
   before_action :set_trip
   before_action :set_vote_totals
 
   def vote
-    @date = Date.parse(params[:date])
     @vote = @itinerary_item.itinerary_votes.find_by(user_id: current_user.id)
     if @vote
       update_user_vote
@@ -18,7 +16,7 @@ class ItineraryVotesController < ApplicationController
 
   def update_user_vote
     if @vote.update(vote_params)
-      redirect_to trip_trip_day_path(@trip.id, @date), notice: 'Vote was updated'
+      redirect_to request.referer || fallback_location, notice: 'Vote was updated'
     else
       render 'trips/day', status: :unprocessable_entity
     end
@@ -28,7 +26,7 @@ class ItineraryVotesController < ApplicationController
     @itinerary_vote = @itinerary_item.itinerary_votes.new(vote_params)
     @itinerary_vote.user = current_user
     if @itinerary_vote.save
-      redirect_to trip_trip_day_path(@trip.id, @date), notice: 'Vote was successfully created'
+      redirect_to request.referer || fallback_location, notice: 'Vote was updated'
     else
       render 'trips/day', status: :unprocessable_entity
     end
@@ -41,10 +39,6 @@ class ItineraryVotesController < ApplicationController
 
   def set_trip
     @trip = Trip.find(params[:trip_id])
-  end
-
-  def set_date
-    @date = params[:date]
   end
 
   def set_vote_totals
