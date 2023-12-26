24413331b8a550b9a9ade770603ef9ea6797f096
Chris Hildebrand <hildebrandc94@gmail.com>
2023-12-26 16:11:24 -0500

    fix: Re-implemented itinerary item conflicts

--- app/controllers/itinerary_items_controller.rb
+++ app/controllers/itinerary_items_controller.rb
@@ -2,7 +2,7 @@ class ItineraryItemsController < ApplicationController
   before_action :set_trip, only: %i[index show new edit create update destroy]
   before_action :set_types, only: %i[index new edit update]
   before_action :set_itinerary_item, only: %i[show update edit]
-  # before_action :set_date, only: %i[show edit]
+  before_action :set_date, only: %i[show]
 
   def index
     @itinerary_item = ItineraryItem.new
@@ -13,13 +13,8 @@ class ItineraryItemsController < ApplicationController
     @up_votes = @itinerary_item.itinerary_votes.where(up_vote: true)
     @down_votes = @itinerary_item.itinerary_votes.where(up_vote: false)
     @all_comments = @itinerary_item.comments
-    # @items_by_day = @trip.itinerary_by_day[@date]
-    # @conflicting_items = @items_by_day.select do |item|
-    # next if item.id == @itinerary_item.id
-
-    # item.start_time < @itinerary_item.end_time &&
-    # @itinerary_item.start_time < item.end_time
-    # end
+    @items_by_day = @trip.itinerary_by_day[@date]
+    @conflicting_items = find_confilcts(@items_by_day, @itinerary_item)
   end
 
   def new
@@ -32,7 +27,7 @@ class ItineraryItemsController < ApplicationController
     if @itinerary_item.update(itinerary_item_params)
       # redirect_to params[:return_to] || root_path
       redirect_to trip_itinerary_item_path(@itinerary_item.trip,
-                                           @itinerary_item)
+                                           @itinerary_item, date: params[:date])
     else
       @date_range = (@trip.start_date..@trip.end_date)
       render :edit, status: :unprocessable_entity
@@ -55,11 +50,19 @@ class ItineraryItemsController < ApplicationController
     @itinerary_item.destroy
     date = Date.parse(params[:date])
 
-    # redirect_to params[:return_to] || root_path
     redirect_to trip_trip_day_path(@trip, date), status: :see_other
   end
 
   private
+
+  def find_confilcts(items, current_item)
+    items.select do |item|
+      next if item.id == current_item.id
+
+      item.start_time < current_item.end_time &&
+        current_item.start_time < item.end_time
+    end
+  end
 
   def set_date
     @date = Date.parse(params[:date])
@@ -78,6 +81,8 @@ class ItineraryItemsController < ApplicationController
   end
 
   def itinerary_item_params
-    params.require(:itinerary_item).permit(:creator_id, :item_type_id, :event_name, :address, :start_time, :end_time)
+    params.require(:itinerary_item).permit(:creator_id, :item_type_id,
+                                           :event_name, :address,
+                                           :start_time, :end_time)
   end
 end
