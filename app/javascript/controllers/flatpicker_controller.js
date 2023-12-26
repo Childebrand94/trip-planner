dda80a29c141fb90d49863983836bdcd90c6a7c6
Chris Hildebrand <hildebrandc94@gmail.com>
2023-12-26 10:08:00 -0500

    fix: Itinerary item end time minimum
    Item minimum is now set to 1 min past start time but the user will by
    default be set to 1 hour passed start time.

--- app/javascript/controllers/flatpicker_controller.js
+++ app/javascript/controllers/flatpicker_controller.js
@@ -8,6 +8,8 @@ export default class extends Controller {
         const tripStart = tripContainer.getAttribute('data-trip-start');
         const tripEnd = tripContainer.getAttribute('data-trip-end');
 
+        let endTimeManuallySet = false;
+
         let startTimePicker = flatpickr('.start_time', {
             minDate: tripStart,
             maxDate: tripEnd,
@@ -17,19 +19,27 @@ export default class extends Controller {
             onChange: function(selectedDates, dateStr, instance) {
                 if (selectedDates[0]) {
                     let newMinDate = new Date(selectedDates[0]);
-
-                    newMinDate.setHours(newMinDate.getHours() + 1);
-
+                    newMinDate.setMinutes(newMinDate.getMinutes() + 1);
                     endTimePicker.set('minDate', newMinDate);
+                }
+
+                if (!endTimeManuallySet) {
+                    let defaultEndDate = new Date(selectedDates[0]);
+                    defaultEndDate.setHours(defaultEndDate.getHours() + 1);
+                    endTimePicker.setDate(defaultEndDate, true);
                 }
             }
         });
+
         let endTimePicker = flatpickr('.end_time', {
             minDate: tripStart,
             maxDate: tripEnd,
             enableTime: true,
             allowInput: true,
             altInput: 'M/j: h:i K',
+            onChange: function() {
+                endTimeManuallySet = true;
+            }
         });
 
         flatpickr('.date', {
