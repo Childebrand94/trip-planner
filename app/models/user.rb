8904c1886790e31be338863a4254684d39a64c26
Chris Hildebrand <hildebrandc94@gmail.com>
2024-01-01 11:06:51 -0500

    fix: Added dependents to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,44 @@
 class User < ApplicationRecord
-  has_many :user_trips
-  has_many :trips, through: :user_trips
-
-  has_many :itinerary_items, foreign_key: 'creator_id', class_name: 'ItineraryItem'
-  has_many :itinerary_votes
-  has_many :comments, foreign_key: 'author_id'
-
-  has_many :debtors
-  has_many :expenses
-  has_many :expenses, through: :debtors
-
-  has_many :invitiations, class_name: 'Invite', foreign_key: 'recipient_id'
-  has_many :sent_invites, class_name: 'Invite', foreign_key: 'sender_id'
-
   has_secure_password
 
-  validates_presence_of :email
-  normalizes :email, with: ->(email) { email.strip.downcase }
-  validates_uniqueness_of :email
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_many :user_trips, dependent: :destroy
+  has_many :trips, through: :user_trips
+
+  has_many :created_itinerary_items,
+           foreign_key: 'creator_id',
+           class_name: 'ItineraryItem',
+           dependent: :destroy
+  has_many :itinerary_votes, dependent: :destroy
+  has_many :authored_comments,
+           foreign_key: 'author_id',
+           class_name: 'Comment',
+           dependent: :destroy
+
+  has_many :debtors, dependent: :destroy
+  has_many :expenses, dependent: :destroy
+  has_many :debtor_expenses,
+           through: :debtors,
+           source: :expense
+
+  has_many :received_invitations,
+           class_name: 'Invite',
+           foreign_key: 'recipient_id',
+           dependent: :destroy
+  has_many :sent_invites,
+           class_name: 'Invite',
+           foreign_key: 'sender_id',
+           dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  before_validation :normalize_email
 
   def voted_on?(itinerary_item)
     itinerary_votes.exists?(itinerary_item_id: itinerary_item.id)
+  end
+
+  private
+
+  def normalize_email
+    self.email = email.strip.downcase if email.present?
   end
 end
