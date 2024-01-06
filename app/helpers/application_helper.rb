e58cb22098371535f9320cf4d6c5d2bf6d8fe6bf
Chris Hildebrand <hildebrandc94@gmail.com>
2024-01-06 15:21:46 -0500

    wip: Styling overview and created footer for mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,17 @@ module ApplicationHelper
   def render_trip_itinerary_footer?
     %w[itinerary_items expenses debts user_trips].include?(controller_name)
   end
+
+  def navigation_items(trip)
+    nav_items = [
+      { title: 'Overview', path: trip_path(trip) },
+      { title: 'Itinerary', path: trip_itinerary_items_path(trip) },
+      { title: 'Expenses', path: trip_expenses_path(trip) },
+      { title: 'My Debts', path: trip_debtors_path(trip) }
+    ]
+
+    nav_items << { title: 'Manage Members', path: trip_user_trips_path(trip) } if admin_for_trip?(trip)
+
+    nav_items
+  end
 end
