1f9bd9b49e8a254e28ce283e6349273db644181a
Chris Hildebrand <hildebrandc94@gmail.com>
2024-01-06 08:18:11 -0500

    wip: Styling website

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
     user_trip = current_user.user_trips.find_by(trip:)
     user_trip&.user_trip_role&.role == ROLES['Admin']
   end
+
+  def render_trip_itinerary_footer?
+    %w[itinerary_items expenses debts user_trips].include?(controller_name)
+  end
 end
