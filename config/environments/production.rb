9ecebda679cb37d535182898dd46290b1a7c2a35
Chris Hildebrand <hildebrandc94@gmail.com>
2024-01-14 18:33:20 -0500

    Changed action mailer smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,11 +96,12 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  config.action_mailer.default_url_options = { host: 'hildebrand-pathfindr-8907d282d8a0.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'hildebrand-pathfindr-8907d282d8a0.herokuapp.com',
+    domain: 'gmail.com',
     user_name: ENV['EMAIL_USERNAME'],
     password: ENV['EMAIL_PASSWORD'],
     authentication: 'plain',
