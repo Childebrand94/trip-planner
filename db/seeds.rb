3483c8f0d9bb147eaf3f860409c9edb64d4ee6a6
Chris Hildebrand <hildebrandc94@gmail.com>
2024-01-15 16:57:08 -0500

    feat: Finished seed for demo user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,6 +45,10 @@ user_roles.each do |r|
 end
 
 # demo data
+ItineraryVote.delete_all
+Comment.delete_all
+Debtor.delete_all
+Expense.delete_all
 ItineraryItem.delete_all
 Note.delete_all
 UserTrip.delete_all
@@ -58,11 +62,17 @@ user_password = 'password'
   User.find_or_create_by(email:) do |user|
     user.first_name = Faker::Name.first_name
     user.last_name = Faker::Name.last_name
-    user.display_name = Faker::FunnyName.name
+    user.display_name = "demo_user_#{i}"
     user.password = user_password
     user.email_confirmed = true
   end
 end
+
+demo_user_1 = User.find_by(email: 'demo_user_1@example.com')
+demo_user_2 = User.find_by(email: 'demo_user_2@example.com')
+demo_user_3 = User.find_by(email: 'demo_user_3@example.com')
+demo_user_4 = User.find_by(email: 'demo_user_4@example.com')
+demo_admin = User.find_by(email: 'demo_user_5@example.com')
 
 # Create a demo trip
 demo_trip = Trip.create!(
@@ -73,11 +83,13 @@ demo_trip = Trip.create!(
   end_date: Date.tomorrow + 1.year + 4.days
 )
 
-(1..5).each do |i|
-  user = User.find_by(email: "demo_user_#{i}@example.com")
-  UserTrip.create!(user:, trip: demo_trip, user_trip_role_id: 1) if user
-end
+# Create admin for trip
+UserTrip.create!(user: demo_admin, trip: demo_trip, user_trip_role_id: 1)
 
+# Add users to trip
+[demo_user_1, demo_user_2, demo_user_3, demo_user_4].each do |user|
+  UserTrip.create!(user:, trip: demo_trip, user_trip_role_id: 2)
+end
 # Example notes
 notes = [
   'Booked a suite at the Bellagio Hotel with a view of the fountains. Check-in is at 3 PM.',
@@ -85,13 +97,11 @@ notes = [
   "Tickets booked for 'O' by Cirque du Soleil at the Bellagio at 7 PM. Pick up tickets at the box office by 6:30 PM."
 ]
 
-user = User.find_by(email: 'demo_user_1@example.com')
-
 # Create notes for the demo trip
 notes.each do |note_body|
   Note.create!(
     body: note_body,
-    author_id: user.id,
+    author_id: demo_admin.id,
     trip_id: demo_trip.id
   )
 end
@@ -117,6 +127,13 @@ itinerary_items = [
     address: 'Paris Las Vegas Hotel & Casino, Las Vegas, NV, USA',
     start_time: demo_trip.start_date.to_time + 7.hours,
     end_time: demo_trip.start_date.to_time + 9.hours,
+    item_type: 1
+  },
+  {
+    event_name: 'Dinner in Vegas',
+    address: 'Some Restaurant in Las Vegas, NV, USA',
+    start_time: demo_trip.start_date.to_time + 7.5.hours,
+    end_time: demo_trip.start_date.to_time + 9.5.hours,
     item_type: 1
   },
   {
@@ -156,8 +173,106 @@ itinerary_items.each do |item|
     address: item[:address],
     start_time: item[:start_time],
     end_time: item[:end_time],
-    creator_id: user.id,
+    creator_id: demo_admin.id,
     item_type_id: item[:item_type],
     trip_id: demo_trip.id
   )
+end
+
+expenses = [
+  {
+    name: 'Flights',
+    amount: 550,
+    date: demo_trip.start_date,
+    payer_id: demo_user_1.id,
+    trip_id: demo_trip.id,
+    expense_category_id: 3
+  },
+  {
+    name: 'Golf',
+    amount: 100,
+    date: demo_trip.start_date + 1.day,
+    payer_id: demo_user_1.id,
+    trip_id: demo_trip.id,
+    expense_category_id: 8
+  },
+  {
+    name: 'Hotel Booking',
+    amount: 2500,
+    date: demo_trip.start_date - 1.day,
+    payer_id: demo_admin,
+    trip_id: demo_trip.id,
+    expense_category_id: 2
+  },
+  {
+    name: 'Restaurant Dinner',
+    amount: 75,
+    date: demo_trip.start_date + 2.days,
+    payer_id: demo_user_1.id,
+    trip_id: demo_trip.id,
+    expense_category_id: 2
+  },
+  {
+    name: 'Museum Tickets',
+    amount: 50,
+    date: demo_trip.start_date + 3.days,
+    payer_id: demo_user_1.id,
+    trip_id: demo_trip.id,
+    expense_category_id: 8
+  },
+  {
+    name: 'Transportation',
+    amount: 200,
+    date: demo_trip.start_date + 1.day,
+    payer_id: demo_user_1.id,
+    trip_id: demo_trip.id,
+    expense_category_id: 4
+  },
+  {
+    name: 'Souvenirs',
+    amount: 50,
+    date: demo_trip.start_date + 4.days,
+    payer_id: demo_user_1.id,
+    trip_id: demo_trip.id,
+    expense_category_id: 6
+  }
+]
+
+expenses.each do |exp|
+  Expense.create!(
+    name: exp[:name],
+    amount: exp[:amount],
+    date: exp[:date],
+    payer_id: demo_user_1.id,
+    trip_id: exp[:trip_id],
+    expense_category_id: exp[:expense_category_id]
+  )
+end
+
+hotel_expense = Expense.find_by(name: 'Hotel Booking')
+
+Debtor.create!(
+  amount_owed: 700,
+  expense_id: hotel_expense.id,
+  user_id: demo_user_1.id
+)
+
+items = ItineraryItem.where(trip_id: demo_trip.id)
+users = [demo_user_1, demo_user_2, demo_user_3, demo_user_4]
+
+items.each do |item|
+  Comment.create!(
+    body: 'Comments main purpose is to help decide on what to plan if items conflict',
+    author_id: demo_admin.id,
+    itinerary_item_id: item.id
+  )
+
+  users.each do |u|
+    random_up_vote = [true, false].sample
+    ItineraryVote.create!(
+      up_vote: random_up_vote,
+      itinerary_item_id: item.id,
+      user_id: u.id
+    )
+  end
 end
